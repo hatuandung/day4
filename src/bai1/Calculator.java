c07faf46b9a034dfd81dc17eff00ab2b19b2829e
Ha Tuan Dung <ha.dung792@gmail.com>
2019-10-12 11:51:26 +0700

    commit

--- src/bai1/Calculator.java
+++ src/bai1/Calculator.java
@@ -1,58 +1,66 @@
 package bai1;
 
 public class Calculator {
-    public int number1 ;
-    public int number2 ;
+    public int number1;
+    public int number2;
 
-    public Calculator(){
+    public Calculator() {
 
     }
+
     public Calculator(int number1, int number2) {
         this.number1 = number1;
         this.number2 = number2;
     }
+
     //sum
-    public int sum(){
+    public int getSum() {
         return number1 + number2;
     }
 
-    public int sum(int number1, int number2){
+    public int getSum(int number1, int number2) {
         return number1 + number2;
     }
+
     //diff
-    public int diff(){
+    public int getDiff() {
         return number1 - number2;
     }
-    public int diff(int number1, int number2){
+
+    public int getDiff(int number1, int number2) {
         return number1 - number2;
     }
+
     //product
-    public int product(){
+    public int getProduct() {
         return number1 * number2;
     }
-    public int product(int number1, int number2){
+
+    public int getProduct(int number1, int number2) {
         return number1 * number2;
     }
+
     //quotient
-    public double quotient(){
-        return (double) number1 / number2;
-    }
-    public double quotient(int number1, int number2){
+    public double getQuotient() {
         return (double) number1 / number2;
     }
 
-    public void output(){
-        System.out.println("Sum: " + sum());
-        System.out.println("Diff " + diff());
-        System.out.println("Product " + product());
-        System.out.println("Quotient " + quotient());
+    public double getQuotient(int number1, int number2) {
+        return (double) number1 / number2;
     }
 
-    public void output(int number1, int number2){
-        System.out.println("Sum: " + sum(number1, number2));
-        System.out.println("Diff " + diff(number1, number2));
-        System.out.println("Product " + product(number1, number2));
-        System.out.println("Quotient " + quotient(number1, number2));
+    public void output() {
+        System.out.println("Sum: " + getSum());
+        System.out.println("Diff " + getDiff());
+        System.out.println("Product " + getProduct());
+        System.out.println("Quotient " + getQuotient());
+    }
+
+    public void output(int number1, int number2) {
+        System.out.println("Sum: " + getSum(number1, number2));
+        System.out.println("Diff " + getDiff(number1, number2));
+        System.out.println("Product " + getProduct(number1, number2));
+        System.out.println("Quotient " + getQuotient(number1, number2));
     }
 
 }
